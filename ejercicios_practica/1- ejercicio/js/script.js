da9f643ca7e53437e26c9230474cb33790c06a4a
velaquia <32940917+velaquia@users.noreply.github.com>
2023-09-18 12:28:22 -0300

    Add files via upload

--- ejercicios_practica/1- ejercicio/js/script.js
+++ ejercicios_practica/1- ejercicio/js/script.js
@@ -15,7 +15,7 @@ todos juntos.
 
 // seleccione el ejercicio a ejecutar
 
-let ejercicio = 0;
+let ejercicio = 5;
 
 if (ejercicio == 0) {
     ejercicio = prompt(`Bienvenido, ingrese el número de ejercicio que quiere realizar`);
@@ -24,24 +24,24 @@ if (ejercicio == 0) {
 // Ejercicios de sintaxis de "for"
 if (ejercicio == 1) {
     // Descomentar cada bloque y encontrar el error de sintaxis :D
-    /*for(i=0;<10i++){
+    /*for (let i=0; i < 10; i++){
         console.log(`el valor de "i" es: ${i}`);
     }
     */
-       /*forar i=0;<10;i++){
+    /*   for (let i=0; i < 10; i++){
         console.log(`el valor de "i" es: ${i}`);
     }
     */
-       /*for(var i=0;0<10;i++){
+      /*for(let i=0; i < 10; i++){
         console.log(`el valor de "i" es: ${i}`);
     }
     */
-       /*for(var i=0;i>10;i++){
+     /* for(let i=0; i < 10; i++){
         console.log(`el valor de "i" es: ${i}`);
     }
     */
-       /*for(var i=o;i<1o;i++){
-        consolelog(`el valor de "i" es: ${i}`);
+     /* for(let i=0; i < 10; i++){
+        console.log(`el valor de "i" es: ${i}`);
     }
     */
 
@@ -53,6 +53,9 @@ if (ejercicio == 2) {
     // En cada iteración del bucle imprimir en consola el número
     // o índice que irá incrementando
     // Utilice bucle "for"
+    for (let i=0; i <= 9; i++){
+        console.log(`El valor del iterador es: ${i}`);
+    }
 }
 
 // Ejercicios de práctica numérica con bucle for of
@@ -62,6 +65,9 @@ if (ejercicio == 3) {
     // y en cada iteración imprira en la consola el valor
     // del número recorrido
     // Utilice bucle "for of"
+    for(const numero of numeros){
+        console.log(numero)
+    }
 }
 
 // Ejercicios de práctica de objetos con bucle for of
@@ -84,6 +90,9 @@ if (ejercicio == 4) {
     // personas.
     // En cada iteración imprimir el nombre de la persona.
     // Utilice bucle "for of"
+    for(const persona of personas){
+        console.log(`El nombre es ${persona.nombre} y el apellido es ${persona.apellido}`);
+    }
 
 }
 
@@ -98,4 +107,14 @@ if (ejercicio == 5) {
     // El bucle deberá ejecutarse por 2 iteraciones.
     // Al terminarse el bucle (luego de ejecutarse todas las iteraciones)
     // deberá imprimir en consola el contenido del array "personas"
+    for (let i=0; i<2; i++){
+        let persona = {
+            "nombre": undefined,
+            "apellido": undefined, 
+        };
+        persona.nombre = prompt("Ingrese un nombre");
+        persona.apellido = prompt("Ingrese un apellido");
+        personas.push(persona);
+    }
+    console.log(personas);    
 }
